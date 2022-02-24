2bff7de6550c6449861f9c881da944afcf903c96
Keith Solomon <keith@solowebdesigns.net>
2022-02-24 10:05:24 -0600

    Update README, add Portainer update script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Random Scripts
+
+A collection of random scripts I use on various platforms.
+
+## Contents
+
+* Portainer upgrade script
---
+++ port-up.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo 'Stop and remove existing container'
+docker stop portainer
+docker rm portainer
+echo ''
+
+echo 'Pull new version'
+docker pull portainer/portainer-ce
+echo ''
+
+echo 'Start new version'
+docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainder_data:/data cr.portainer.io/portainer/portainer-ce:latest
